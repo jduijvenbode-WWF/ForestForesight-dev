157ad9f435fca258e1ab6befdcafa37233fa97cb
jvduijvenbode <jonas@geotics.nl>
2024-05-20 09:41:53 +0200

    Update 2024 monthly processing.R

--- scripts_jonas/2024 monthly processing.R
+++ scripts_jonas/2024 monthly processing.R
@@ -3,8 +3,6 @@ library(sf)
 data(countries)
 ff_folder="C:/data/storage"
 proc_date <- "2024-05-01"
-train_start= "2023-05-01"
-train_end= "2023-10-01"
 countrynames=countries$iso3
 
 proc_dates=rev(as.character(daterange("2023-06-01","2024-05-01")))
@@ -24,17 +22,16 @@ for(proc_date in proc_dates){
       tryCatch({
         b <- train_predict_raster(shape = shape,
                                   prediction_date = proc_date,
-                                  train_start = train_start,
-                                  train_end = train_end,
                                   ff_folder = ff_folder,
                                   verbose = TRUE,
                                   model = modelpath)
+        terra::writeRaster(b,paste0(country,"_",proc_date,".tif"),overwrite = T)
       }, error = function(e) {
         # Print the error message
         print(paste("An error occurred:", e))
         # Continue the loop or execute other code as needed
       })
-      terra::writeRaster(b,paste0(country,"_",proc_date,".tif"),overwrite = T)
+
 
     }
   }
