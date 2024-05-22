8e41fecb81c9efc8d8f6f835df1d5528f0cbc0aa
jvduijvenbode <jonas@geotics.nl>
2024-05-22 21:15:50 +0200

    simplified coordinate system to fix wrong overlaps

--- scripts_jonas/20240325 contextualization per country.R
+++ scripts_jonas/20240325 contextualization per country.R
@@ -4,23 +4,22 @@ create_grid <- function(poly,num_rows,num_cols) {
   # Calculate the bounding box of the polygon
 
   # Create the grid
-  grid <- vect(st_make_grid(poly, n = c(num_cols, num_rows), what = "polygons"))
+  grid <- terra::vect(st_make_grid(poly, n = c(num_cols, num_rows), what = "polygons"))
 
   return(grid)
 }
 data(countries)
 setwd("C:/data")
 arcpy_location='\"C:/Program Files/ArcGIS/Pro/bin/Python/envs/arcgispro-py3/python.exe\"'
-script_location='C:/data/git/ForestForesight/scripts_jonas/tilepackager/map_tile_package.py'
+script_location='C:/data/git/ForestForesight-dev/scripts_jonas/tilepackager/map_tile_package.py'
 ff_folder="C:/data/storage"
 proc_date <- "2024-05-01"
 train_start= "2023-05-01"
 train_end= "2023-10-01"
-countrynames=c("Laos","Colombia","Gabon","Sarawak","Bolivia","Madre de Dios","Suriname","Peru","Kalimantan")
-isos=c("LAO","COL","GAB","MYS","BOL","PER","SUR","PER","IDN")
+countrynames=c("Burundi","Laos","Colombia","Gabon","Sarawak","Bolivia","Madre de Dios","Suriname","Peru","Kalimantan")
+isos=c("BDI","LAO","COL","GAB","MYS","BOL","PER","SUR","PER","IDN")
 gadm_s <- terra::vect("C:/data/storage/contextualization/GADM.gpkg")
-for(x in seq(length(countrynames))){
-
+for(x in seq(length(countrynames))[1]){
   country <- countrynames[x]
   cat(paste("processing",country,"\n"))
   countryiso <- isos[x]
@@ -30,16 +29,18 @@ for(x in seq(length(countrynames))){
   if(!file.exists(paste0(country,"_",proc_date,".tif"))){
     #shape <- terra::vect(countries)[which(countries$group==country),]
     shape <- terra::vect(countries)[which(countries$iso3 == countryiso),]
-    if(country=="Sarawak"){shape=disagg(shape)[9]}
-    if(country=="Kalimantan"){shape=disagg(shape)[133]}
+    if(country=="Sarawak"){shape=terra::disagg(shape)[9]}
+    if(country=="Kalimantan"){shape=terra::disagg(shape)[133]}
     if(country=="Madre de Dios"){shape=aggregate(gadm_s[which(gadm_s$"NAME_1" == "Madre de Dios"),])}
     modelname=countries$group[which(countries$iso3==countryiso)]
     modelpath=file.path(ff_folder,"models",modelname,paste0(modelname,".model"))
     if(!file.exists(modelpath)){stop(paste(modelpath,"does not exist"))}
     #b <- ForestForesight::train_predict_raster(shape = shape,prediction_date = proc_date,train_start = train_start,train_end=train_end,ff_folder = ff_folder,verbose = T)
-    b <- train_predict_raster(shape = shape,prediction_date = proc_date,train_start = train_start,train_end=train_end,ff_folder = ff_folder,verbose = T,model=modelpath)
-    terra::writeRaster(b,paste0(country,"_",proc_date,".tif"),overwrite = T)
-    #b <- terra::rast(paste0(country,"_2024-01-01.tif"))
+    # b <- train_predict_raster(shape = shape,prediction_date = proc_date,train_start = train_start,train_end=train_end,ff_folder = ff_folder,verbose = T,model=modelpath)
+    # terra::writeRaster(b,paste0(country,"_",proc_date,".tif"),overwrite = T)
+    shape=terra::project(shape,"epsg:3857")
+    b <- terra::project(terra::rast(file.path(ff_folder,"predictions",countryiso,paste0(countryiso,"_",proc_date,".tif"))),"epsg:3857")
+    if(country %in% c("Sarawak","Kalimantan","Madre de Dios")){b=terra::crop(terra::mask(b,shape),shape)}
 
     #########dashboard version 2########
 
@@ -69,7 +70,8 @@ for(x in seq(length(countrynames))){
     ecobiome$ecobiome <- paste(ecobiome$Biome,ecobiome$Ecoregion,sep = "_")
     wdpa2 <- aggregate(wdpa)
     wdpa <- terra::intersect(wdpa, wdpa2)
-    nonwdpa <- terra::as.polygons(terra::ext(wdpa)) - wdpa
+    if(length(wdpa)>0){nonwdpa <- terra::as.polygons(terra::ext(wdpa)) - wdpa}else{nonwdpa <- terra::as.polygons(terra::ext(wdpa))}
+
     nonwdpa$status = "not protected"
     wdpa$status = "protected"
     wdpa <- rbind(wdpa, nonwdpa)
@@ -84,11 +86,11 @@ for(x in seq(length(countrynames))){
     colras[colras < 0.50] <- NA
     colras <- terra::crop(colras,gadm)
     threshold <- as.numeric(terra::global(colras,fun = quantile,probs = 0.90,na.rm = T))
-    colrasproj <- terra::project(colras,"epsg:3857")
-    names(colrasproj) <- paste0("Forest Foresight predictions",country)
-    terra::writeRaster(colrasproj,paste0(country,".tif"),overwrite = T)
-    all2proj <- terra::project(all2,"epsg:3857")
-    vals <- terra::extract(colras > 0, all2proj, fun = "sum", na.rm = T)[, 2]
+
+    names(colras) <- paste0("Forest Foresight predictions",country)
+    terra::writeRaster(colras,paste0(country,".tif"),overwrite = T)
+
+    vals <- terra::extract(colras > 0, all2, fun = "sum", na.rm = T)[, 2]
     vals[is.na(vals)] <- 0
     highalertvals <- terra::extract(colras > threshold, all2, fun = "sum", na.rm = T)[, 2]
     highalertvals[is.na(highalertvals)] <- 0
@@ -117,10 +119,9 @@ for(x in seq(length(countrynames))){
 
 
     # Convert each part to polygons
-    extpols=create_grid(ext(colras2),2,2)
+    extpols=create_grid(terra::ext(colras2),2,2)
     # Combine the resulting polygons
-    newras=terra::project(colras2, terra::crs(all2))
-    pollist=sapply(1:length(extpols),function(x) terra::as.polygons(crop(newras,extpols[x,]), dissolve = F))
+    pollist=sapply(1:length(extpols),function(x) terra::as.polygons(terra::crop(newras,extpols[x,]), dissolve = F))
     pols <- do.call(rbind, pollist)
 
 
@@ -130,33 +131,33 @@ for(x in seq(length(countrynames))){
     pols2 = cbind(pols, polvals)
     pols2$x = as.numeric(round(terra::crds(terra::centroids(pols2))[,1],3))
     pols2$y = as.numeric(round(terra::crds(terra::centroids(pols2))[,2],3))
-    terra::writeVector(terra::project(pols2,"epsg:3857"), paste0(country,"_highalerts.shp"),overwrite = T)
+    terra::writeVector(pols2, paste0(country,"_highalerts.shp"),overwrite = T)
     #code to extract the high alert values and amount of normal alerts for wdpa, ecobiome and gadm separately
     wdpa2 = terra::intersect(wdpa, aggregate(gadm))
-    eventcount = terra::project(colras > 0,terra::crs(ecobiome))
+    eventcount = colras > 0
     wdpa2$events <- terra::extract(eventcount,wdpa2,fun = "sum",na.rm = T)[,2]
     wdpa2$events[which(is.na(wdpa2$events))] = 0
     wdpa2$x = as.numeric(round(terra::crds(terra::centroids(wdpa2))[,1],3))
     wdpa2$y = as.numeric(round(terra::crds(terra::centroids(wdpa2))[,2],3))
-    terra::writeVector(terra::project(wdpa2,"epsg:3857"), paste0(country,"_wdpa.shp"),overwrite = T)
+    terra::writeVector(wdpa2, paste0(country,"_wdpa.shp"),overwrite = T)
 
     ecobiome2 <- terra::intersect(ecobiome, aggregate(gadm))
     ecobiome2$events <- terra::extract(eventcount,ecobiome2,fun = "sum",na.rm = T)[,2]
     ecobiome2$events[which(is.na(ecobiome2$events))] <- 0
     ecobiome2$x <- as.numeric(round(terra::crds(terra::centroids(ecobiome2))[,1],3))
     ecobiome2$y <- as.numeric(round(terra::crds(terra::centroids(ecobiome2))[,2],3))
-    terra::writeVector(terra::project(ecobiome2,"epsg:3857"), paste0(country,"_ecobiome.shp"),overwrite = T)
+    terra::writeVector(ecobiome2, paste0(country,"_ecobiome.shp"),overwrite = T)
     names(gadm) <- c("country","province","district","municipality")
 
     gadm$events <- terra::extract(eventcount,gadm,fun = "sum",na.rm = T)[,2]
     gadm$events[which(is.na(gadm$events))] <- 0
-    gadm$highevents <- round(terra::extract(terra::project(colras2,gadm),gadm,fun = "sum",na.rm = T)[,2])
+    gadm$highevents <- round(terra::extract(colras2,gadm,fun = "sum",na.rm = T)[,2])
     gadm$highevents[which(is.na(gadm$highevents))] <- 0
     gadm$label <- paste0(gadm$district,"\n",gadm$municipality)
     head(gadm)
     gadm$x = as.numeric(round(terra::crds(terra::centroids(gadm))[,1],3))
     gadm$y = as.numeric(round(terra::crds(terra::centroids(gadm))[,2],3))
-    terra::writeVector(terra::project(gadm,"epsg:3857"), paste0(country,"_gadm.shp"),overwrite = T)
+    terra::writeVector(gadm, paste0(country,"_gadm.shp"),overwrite = T)
     files <- list.files()
     files <- files[unique(c(grep("dbf$",files),grep("prj$",files),grep("shp$",files),grep("shx$",files),grep("cpg$",files)))]
     if (file.exists(paste0(country,"_contextualization.zip"))) {file.remove(paste0(country,"_contextualization.zip"))}
