1f8cf2d7919a5368eeae792ad96dd958323fd309
jduijvenbode-WWF <149797028+jduijvenbode-WWF@users.noreply.github.com>
2024-06-25 10:08:26 +0200

    add max def for baseline

--- scripts_Zillah/baseline_amounts.R
+++ scripts_Zillah/baseline_amounts.R
@@ -1,14 +1,15 @@
 setwd("D:/ff-dev/results/preprocessed")
-outputfile="../accuracy_analysis/baseline_amounts_alldata.csv"
+outputfile="D:/ff-dev/predictionsZillah/accuracy_analysis/baseline_amounts_alldata.csv"
 library(ForestForesight)
 files=list.files(recursive=T,pattern="groundtruth6m")
 
 data("degree_polygons")
 pols=vect(degree_polygons)
 pols=vect(degree_polygons)
-calculate_scores=function(predfile,groundtruthfile,pols,adddate=T,fmaskfile=NULL){
+calculate_scores=function(predfile,groundtruthfile,pols,adddate=T,fmaskfile=NULL, totdeffile){
   pred=rast(predfile)
   pred[is.na(pred)]=0
+  max_def = 1600-rast(totdeffile,win=ext(pred))
   groundtruth=rast(groundtruthfile,win=ext(pred))
   groundtruth[is.na(groundtruth)]=0
   date=substr(basename(groundtruthfile),10,19)
@@ -18,8 +19,8 @@ calculate_scores=function(predfile,groundtruthfile,pols,adddate=T,fmaskfile=NULL
     fmask=rast(fmaskfile)
     fmask[is.na(fmask)]=0
     fmask=fmask>0
-    se = ((groundtruth - pred)^2)*fmask
-  }else{se = (groundtruth - pred)^2}
+    se = ((groundtruth - min(max_def,pred))^2)*fmask
+  }else{se = (groundtruth - min(max_def,pred))^2}
   pols$rmse <- (terra::extract(se,pols,fun = "mean",na.rm = T,touches = F)[,2])^0.5
   pols$date=date
   pols$tile=tile
@@ -31,9 +32,9 @@ for(x in 1:length(files)){
   cat(paste0(file," (",x," out of",length(files),")\n"))
   blfile=gsub("groundtruth","input",gsub("groundtruth6m","lastsixmonths",file))
   fmaskfile=file.path("input",basename(dirname(file)),paste0(basename(dirname(file)),"_2021-01-01_initialforestcover.tif"))
-
+  totdeffile= gsub("groundtruth","input",gsub("groundtruth6m","totallossalerts",file))
   groundtruthfile=gsub("predictions","groundtruth",file)
-  calcpols=calculate_scores(predfile = blfile,groundtruthfile = groundtruthfile,pols = pols,fmaskfile = fmaskfile)
+  calcpols=calculate_scores(predfile = blfile,groundtruthfile = groundtruthfile,pols = pols,fmaskfile = fmaskfile, todeffile=totdeffile)
   if(x==1){allpols=calcpols}else{allpols=rbind(allpols,calcpols)}
 
 }
