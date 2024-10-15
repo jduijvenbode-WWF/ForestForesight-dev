461f211b8bb35b5045422ffddb5af0d734584b7f
jvduijvenbode <149797028+jduijvenbode-WWF@users.noreply.github.com>
2024-10-15 18:56:53 +0200

    updated tile packager for automatic upload

--- scripts_jonas/20240904 contextualization per country.R
+++ scripts_jonas/20240904 contextualization per country.R
@@ -4,21 +4,16 @@ library(zip)
 
 
 
-create_grid <- function(poly, num_rows, num_cols) {
-  grid <- terra::vect(st_make_grid(poly, n = c(num_cols, num_rows), what = "polygons"))
-  return(grid)
-}
-
 setwd("C:/data/dashboard_data")
 ff_folder <- "C:/data/storage"
-proc_date <- "2024-09-01"
+proc_date <- "2024-10-01"
 arcpy_location <- '\"C:/Program Files/ArcGIS/Pro/bin/Python/envs/arcgispro-py3/python.exe\"'
 script_location <- 'C:/data/git/ForestForesight-dev/scripts_jonas/tilepackager/map_tile_package.py'
 
-countrynames <- c("Laos", "Colombia", "Gabon", "Bolivia", "Peru", "Kalimantan", "Guaviare")
-isos <- c("LAO", "COL", "GAB", "BOL", "PER", "IDN", "COL")
+countrynames <- c("Laos", "Gabon", "Bolivia", "Peru", "Kalimantan", "Guaviare")
+isos <- c("LAO", "GAB", "BOL", "PER", "IDN", "COL")
 
-for (x in seq(length(countrynames))) {
+for (x in rev(seq(length(countrynames)))) {
   country <- countrynames[x]
   cat(paste("Processing", country, "\n"))
   countryiso <- isos[x]
@@ -31,6 +26,7 @@ for (x in seq(length(countrynames))) {
     shape <- terra::vect(countries)[which(countries$iso3 == countryiso),]
     if (country == "Sarawak") {shape <- terra::disagg(shape)[9]}
     if (country == "Kalimantan") {shape <- terra::disagg(shape)[133]}
+    if(country=="Guaviare") {shape=terra::vect("../AOIs/Guaviare.gpkg") }
     shape <- terra::project(shape, "epsg:3857")
 
     b <- terra::project(terra::rast(file.path(ff_folder, "predictions", countryiso, paste0(countryiso, "_", proc_date, ".tif"))), "epsg:3857")
@@ -44,20 +40,16 @@ for (x in seq(length(countrynames))) {
     names(colras) <- paste0("Forest Foresight predictions ", country)
     terra::writeRaster(colras, paste0(country, ".tif"), overwrite = TRUE)
 
-    # Calculate thresholds
-    high_threshold <- as.numeric(terra::global(colras, fun = quantile, probs = 0.70, na.rm = TRUE))
-    highest_threshold <- as.numeric(terra::global(colras, fun = quantile, probs = 0.90, na.rm = TRUE))
-
     # Create risk polygons
-    medium_risk <- ff_polygonize(colras, threshold = 0.5, output_file = paste0(country, "_medium_risk.shp"))
-    high_risk <- ff_polygonize(colras, threshold = high_threshold, output_file = paste0(country, "_high_risk.shp"))
-    highest_risk <- ff_polygonize(colras, threshold = highest_threshold, output_file = paste0(country, "_highest_risk.shp"))
+    medium_risk <- ff_polygonize(colras, threshold = "medium", output_file = paste0(country, "_medium_risk.shp"),verbose = T,calc_max = T)
+    high_risk <- ff_polygonize(colras, threshold = "high", output_file = paste0(country, "_high_risk.shp"),verbose=T,calc_max = T,contain_polygons = medium_risk)
+    highest_risk <- ff_polygonize(colras, threshold = "very high", output_file = paste0(country, "_highest_risk.shp"),verbose=T,calc_max = T, contain_polygons = high_risk)
 
     # Create tpkx
     cat("Creating tpkx\n")
     tpkx_file <- paste0(country, "_", proc_date, ".tpkx")
     if (file.exists(tpkx_file)) {file.remove(tpkx_file)}
-    system(paste(arcpy_location, script_location, paste0('\"', file.path(getwd(), paste0(country, ".tif")), '\"'), paste0('\"', file.path(getwd(), tpkx_file), '\"')))
+    #system(paste(arcpy_location, script_location, paste0('\"', file.path(getwd(), paste0(country, ".tif")), '\"'), paste0('\"', file.path(getwd(), tpkx_file), '\"')))
 
     # Create zip file with shapefiles
     zip_file <- paste0(country, "_risk_areas.zip")
--- scripts_jonas/tilepackager/map_tile_package.py
+++ scripts_jonas/tilepackager/map_tile_package.py
@@ -16,18 +16,20 @@ class TemplateNotFoundError(Exception):
 ws = os.path.dirname(__file__)
 template = os.path.join(ws, r'temps\template.aprx')
 
-temp_lyr = os.path.join(ws, r"temps\lyr.lyrx")
+temp_lyr = os.path.join(ws, r"temps\lyr2.lyrx")
 if not os.path.exists(template) or not os.path.exists(temp_lyr):
     raise TemplateNotFoundError('Template Not Found (folder named temps in same location as python script)')
     
 parser = argparse.ArgumentParser(description='Map tile package script')
 parser.add_argument('image', type=str, help='Input image, should a single band image')
 parser.add_argument('output', type=str, help='Output map tile')
-
+parser.add_argument('country', type=str, help='name of country')
+parser.add_argument('date', type=str, help='date of processing')
 
 args = parser.parse_args()
 image = args.image
 output = args.output
+country = args.country
 
 dirname = os.path.dirname(output)
 basename = os.path.basename(output)
@@ -50,11 +52,11 @@ def is_single_band_continuous_image(raster_path):
             if raster.pixelType.lower() in ['f32', 'f64', 'u32', 's32', 'u16', 's16']:
                 return True
     except Exception as e:
-        raise RasterNotSupportedError('Rater Not Supported')
+        raise RasterNotSupportedError('Raster Not Supported')
     return False
 
 if not is_single_band_continuous_image(image):
-    raise RasterNotSupportedError('Rater Not Supported')
+    raise RasterNotSupportedError('Raster Not Supported')
 
 project = arcpy.mp.ArcGISProject(template)
 active_map = project.listMaps()[0]
@@ -80,4 +82,18 @@ arcpy.management.CreateMapTilePackage(
     package_type="tpkx",
     min_level_of_detail=1,
     create_multiple_packages="CREATE_SINGLE_PACKAGE",
+)
+
+arcpy.management.SharePackage(
+    in_package=output,
+    username="",
+    password='',
+    summary="Predictions of Forest Foresight for "+country+" for "+date,
+    tags="ForestForesight",
+    credits="ForestForesight WWF-NL",
+    public="MYGROUPS",
+    groups="'Forest Foresight'",
+    organization="EVERYBODY",
+    publish_web_layer="FALSE",
+    portal_folder=""
 )
